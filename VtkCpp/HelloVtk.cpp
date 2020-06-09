e7040e1e0aee752ea8d0205d7dc38cf098898878
Sai Adikarla <saik@tataelxsi.co.in>
2020-06-09 16:43:12 +0530

    Updated variable name.

--- VtkCpp/HelloVtk.cpp
+++ VtkCpp/HelloVtk.cpp
@@ -42,27 +42,27 @@ int main(int argc, char *argv[])
     //  specific implementation invoked during runtime.
 
     // Create  Source to produces polygonal data.
-    vtkSmartPointer<vtkCubeSource> coneSource =
+    vtkSmartPointer<vtkCubeSource> cubeSource =
             vtkSmartPointer<vtkCubeSource>::New();
-    coneSource->SetXLength(2.0);
-    coneSource->SetYLength(2.0);
-    coneSource->SetZLength(2.0);
-    coneSource->Update();
+    cubeSource->SetXLength(2.0);
+    cubeSource->SetYLength(2.0);
+    cubeSource->SetZLength(2.0);
+    cubeSource->Update();
 
     // Create  Mapper to map the polygonal data to graphics primitives.
-    vtkSmartPointer<vtkPolyDataMapper> coneMapper =
+    vtkSmartPointer<vtkPolyDataMapper> cubeMapper =
             vtkSmartPointer<vtkPolyDataMapper>::New();
-    coneMapper->SetInputConnection(coneSource->GetOutputPort());
+    cubeMapper->SetInputConnection(cubeSource->GetOutputPort());
 
     // Create  actor to represent the mapper's primitive.
-    vtkSmartPointer<vtkActor> coneActor =
+    vtkSmartPointer<vtkActor> cubeActor =
             vtkSmartPointer<vtkActor>::New();
-    coneActor->SetMapper(coneMapper);
+    cubeActor->SetMapper(cubeMapper);
 
     // Create the Renderer  to assign and drawing actors to it.
     vtkSmartPointer<vtkRenderer> renderer =
             vtkSmartPointer<vtkRenderer>::New();
-    renderer->AddActor(coneActor);
+    renderer->AddActor(cubeActor);
     renderer->SetBackground(0.1, 0.3, 0.1);
 
     // Create the render window  by addign renderer.
